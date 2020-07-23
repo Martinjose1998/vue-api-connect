0f0fe68dd42155aeaa66a8cd285ab0f4f89c9a02
Martinjose1998 <65858159+Martinjose1998@users.noreply.github.com>
2020-07-22 23:41:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = new Vue({
           else
           {
             axios
-            .get('https://api.nutritionix.com/v1_1/search/'+this.search+'?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=f89a9be7&appKey=646af2a6f5221a09202b9346dd3be1d8')
+            .get('https://api.nutritionix.com/v1_1/search/'+this.search+'?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=APPID&appKey=APPKEY')
             .then(response => (this.info = response.data.hits))
           }
         
